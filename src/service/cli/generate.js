eac2b2287e6f81dc6e2fc5a643171dd6a865cbb1
Pavel Klimov <pavelklimovmail@yandex.ru>
2020-02-10 03:08:17 +0300

    add cli

---
+++ src/service/cli/generate.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const fs = require(`fs`);
+const {
+  getRandomInt,
+  shuffle,
+} = require(`../../utils`);
+
+const DEFAULT_COUNT = 1;
+const FILE_NAME = `mocks.json`;
+
+const TITLES = [
+  `Ёлки. История деревьев`,
+  `Как перестать беспокоиться и начать жить`,
+  `Как достигнуть успеха не вставая с кресла`,
+  `Обзор новейшего смартфона`,
+  `Лучше рок-музыканты 20-века`,
+  `Как начать программировать`,
+  `Учим HTML и CSS`,
+  `Что такое золотое сечение`,
+  `Как собрать камни бесконечности`,
+  `Борьба с прокрастинацией`,
+  `Рок — это протест`,
+  `Самый лучший музыкальный альбом этого года`,
+];
+
+const SENTENCES = [
+  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
+  `Первая большая ёлка была установлена только в 1938 году.`,
+  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
+  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
+  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
+  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
+  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
+  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
+  `Программировать не настолько сложно, как об этом говорят.`,
+  `Простые ежедневные упражнения помогут достичь успеха.`,
+  `Это один из лучших рок-музыкантов.`,
+  `Он написал больше 30 хитов.`,
+  `Из под его пера вышло 8 платиновых альбомов.`,
+  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
+  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
+  `Достичь успеха помогут ежедневные повторения.`,
+  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
+  `Как начать действовать? Для начала просто соберитесь.`,
+  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
+  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
+];
+
+const CATEGORIES = [
+  `Деревья`,
+  `За жизнь`,
+  `Без рамки`,
+  `Разное`,
+  `IT`,
+  `Музыка`,
+  `Кино`,
+  `Программирование`,
+  `Железо`,
+];
+
+const getRandomDate = () => {
+  const randomDate = new Date(Date.now() - getRandomInt(0, 3600 * 24 * 1000 * 365));
+  const year = randomDate.getFullYear();
+  const month = (randomDate.getMonth() + 1 < 10) ? `0${randomDate.getMonth() + 1}` : randomDate.getMonth() + 1;
+  const date = (randomDate.getDate() < 10) ? `0${randomDate.getDate()}` : randomDate.getDate();
+  const hours = (randomDate.getHours() < 10) ? `0${randomDate.getHours()}` : randomDate.getHours();
+  const minuts = (randomDate.getMinutes() < 10) ? `0${randomDate.getMinutes()}` : randomDate.getMinutes();
+  const seconds = (randomDate.getSeconds() < 10) ? `0${randomDate.getSeconds()}` : randomDate.getSeconds();
+  return `${year}-${month}-${date} ${hours}:${minuts}:${seconds}`;
+};
+
+const generateOffers = (count) => (
+  Array(count).fill({}).map(() => ({
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    announce: shuffle(SENTENCES).slice(1, 5).join(` `),
+    fullText: shuffle(SENTENCES).slice(1, SENTENCES.length - 1).join(` `),
+    createdDate: getRandomDate(),
+    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+  }))
+);
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    if (countOffer > 1000) {
+      console.error(`Не более 1000 объявлений`);
+      return;
+    }
+    const content = JSON.stringify(generateOffers(countOffer));
+    fs.writeFile(FILE_NAME, content, (err) => {
+      if (err) {
+        return console.error(`Can't write data to file...`);
+      }
+
+      return console.info(`Operation success. File created.`);
+    });
+  }
+};
